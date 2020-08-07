38608864252dbce5b2532fd1958e1ab7907cb869
ZaveDev <66022270+ZaveDev@users.noreply.github.com>
2020-08-07 13:55:05 -0400

    MVP attempting Stretch

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -13,18 +14,13 @@ const initialValue = {
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+const CheckoutForm = () => {
+  const [
+    showSuccessMessage,
+    values,  
+    handleChanges,
+    handleSubmit
+  ] = useForm(false, initialValue)
 
   return (
     <>
@@ -56,15 +52,24 @@ const CheckoutForm = (props) => {
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input 
+            name="city" 
+            value={values.city} 
+            onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input 
+            name="state" 
+            value={values.state} 
+            onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input 
+            name="zip" 
+            value={values.zip} 
+            onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -8,10 +8,37 @@ export default class PlantList extends Component {
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
+  componentDidMount(){
+    axios
+      .get("http://localhost:3333/plants")
+      .then(res => {
+        this.setState({plants: res.data.plantsData })
+      })
+      .catch(err => {console.log(err);})
+  }
+  componentDidUpdate(){
+
+  }
+  // searchFilteredPlants = (searchedPlant) => {
+  //   axios
+  //   .get("http://localhost:3333/plants")
+  //   .then(res => {
+  //     res.data.plantsData.filter(currentPlant => {
+  //       if (searchedPlant == currentPlant.name) {
+  //         return 
+  //       }
+  //     })
+  //     this.setState({plants:  })
+  //   })
+  //   .catch(err => {console.log(err);})
+  // }
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
       <main className="plant-list">
+        <div className="searchbar">
+          <input type="text" placeholder="Search"/><button>Search</button>
+        </div>
         {this.state?.plants?.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
